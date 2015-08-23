8cff67ba3bc4e8dda130f7c3e39661814f0c4036
wiley <wiley@alltheducks.com>
2015-08-23 15:24:13 +1000

    initial implementation of course event handler for stub

---
+++ src/main/java/edu/myinst/extensions/CourseEventHandler.java
@@ -0,0 +1,43 @@
+package edu.myinst.extensions;
+
+import blackboard.admin.persist.course.CloneConfig;
+import blackboard.admin.persist.course.CourseEventListener;
+import blackboard.persist.Id;
+
+
+/**
+ * This Class handles course create and course clone events.
+ *
+ * A course create event occurs when a course is created.
+ *
+ * A course clone event occurs when course content is copied from one course to another.
+ *
+ */
+public class CourseEventHandler implements CourseEventListener {
+
+    /**
+     * executed by the blackboard.cmsadmin.postCloneCourseEventListener extension point.
+     *
+     * This method is called after a course copy operation.  This might be a full course copy, or
+     * it might only be a copy of a subset of course content.
+     *
+     * @param sourceId The Id of the source course.
+     * @param targetId The Id of the target course
+     * @param cfg The CloneConfig object which was used when cloning the course.
+     * @throws Exception
+     */
+    public void courseCloned(Id sourceId, Id targetId, CloneConfig cfg) throws Exception {
+        System.out.println("Course Cloned. Source ID: " + sourceId.toExternalString() + " Target ID: " + targetId + " Clong Config: " + cfg.getLoggedOnUser());
+    }
+
+    /**
+     * executed by the blackboard.cmsadmin.postCreateCourseEventListener extension point.
+     *
+     * This method is called after a course is created.
+     *
+     * @param courseId the Id of the newly created course.
+     */
+    public void courseCreated(Id courseId) {
+        System.out.println("Course Created ID: " + courseId.toExternalString());
+    }
+}
